5ecbea9f2013933368d76c40de5eea77c1879ef3
dwusiq <dwusiq@163.com>
2017-07-06 18:14:06 +0800

    spring定时器 quartz

---
+++ springQuartz/src/main/java/com/wusiq/task/MyTask.java
@@ -0,0 +1,12 @@
+package com.wusiq.task;
+
+import java.util.Date;
+
+/**
+ * 任务类
+ */
+public class MyTask {
+    public void doSomeTask(){
+        System.out.println(new Date()+"打印了一次！");
+    }
+}
